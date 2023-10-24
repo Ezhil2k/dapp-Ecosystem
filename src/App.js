30e4f4f76b80b7f08b394456c56310d1e2a226cf
Ezhil2k <ezhil17vr@gmail.com>
2023-10-24 22:30:00 +0530

    ERC20 token done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,160 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Web3Modal from 'web3modal'; 
+import {providers, Contract, ethers} from 'ethers';
+import {useEffect, useState, useRef} from 'react'
+import { DOGERINU_CONTRACT_ADDRESS, abi  } from './helpers/constants';
+import {Toaster, toast} from 'react-hot-toast'
 function App() {
+  
+  const [walletconnected,setWalletConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState("");
+  const [accountBalance, setAccountBalance] = useState("");
+  const [transferTo, setTransferTo] = useState("");
+  const [transferAmount, setTransferAmount] = useState(0);
+
+  const [sending,setSending] = useState(false)
+
+
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => { 
+
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    //get the chainId and check if it is the network we want
+    const {chainId} = await web3Provider.getNetwork();
+    console.log(chainId)
+    if (chainId !== 11155111) {
+      alert("change network to sepolia");
+      throw new Error("change network to sepolia");
+    }
+
+    // more priveleged version of the web3Provider that can also send txn to the blockchain
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+
+    return web3Provider;
+  }
+
+  const handleConnect = async () => {
+    try{
+      const thesigner = await getProviderOrSigner(true);
+      const currentWalletAddress = await thesigner.getAddress();
+
+      setWalletAddress(currentWalletAddress);
+      setWalletConnected(true);
+      getAccountBalance(currentWalletAddress);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  const handleDisconnect = () => {
+    setWalletConnected(false);
+    setWalletAddress("");
+  }
+
+  const getAccountBalance = async (address) => {
+    try {
+      const provider = await getProviderOrSigner();
+      const tokenContract = new Contract(
+        DOGERINU_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+
+      const _accountBalance = await tokenContract.balanceOf(address.toString());
+      setAccountBalance(ethers.utils.formatEther(_accountBalance.toString()));
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const sendTokens = async (e) => {
+    e.preventDefault();
+
+    if (parseInt(transferAmount) <= 0){
+      toast.error("Amount must be greater than 0");
+      return;
+    }
+
+    if(transferTo === "") {
+      toast.error("Address field is empty");
+      return;
+    }
+
+    const amountToTransfer = ethers.utils.parseEther(transferAmount.toString());
+
+    try{
+      const signer = await getProviderOrSigner(true);
+      const tokenContract = new Contract(
+        DOGERINU_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+
+      const transferStatus = await tokenContract.transfer(transferTo, amountToTransfer);
+      setSending(true);
+      await transferStatus.wait();
+      setSending(false);
+
+      const etherScanLink = `https://sepolia.etherscan.io/tx/${transferStatus.hash}`;
+      toast.success((t) => (
+        <p>Transaction success! check status here: <a rel="noreferrer" target="_blank" href= {etherScanLink}>view block explorer</a></p>
+      ), {
+        duration: 10000
+      });
+
+      getAccountBalance(walletAddress);
+      
+    } catch (err) {
+      toast.error("Error sending tokens.")
+    }
+  }
+
+  useEffect(() => {
+    if(!walletconnected){
+      web3ModalRef.current = new Web3Modal({
+        network: "sepolia",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      })
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Toaster/>
+      <header className='App-header'>
+        <h3>Doger Inu</h3>
+        <div className='wrapperDiv'>
+        
+        {walletconnected ? (
+          <>
+          <b>Balance:</b> {accountBalance} DGI
+
+          <form className='transferForm' onSubmit={(e) => sendTokens(e)}>
+            <div className='formLeft'>
+              <input type="text" name='toAddress' value={transferTo} onChange={(e) => setTransferTo(e.target.value)} placeholder="To address"/>
+              <input min="0" type='number' name='transferAmount' value={transferAmount} onChange={(e) => setTransferAmount(e.target.value)} placeholder='Amount'/>
+            </div>
+            <div className='formRight'>
+              <button type='submit'>{sending ? 'sending...' : 'send Tokens'}</button>
+            </div>
+          </form>
+
+          <button className='button' style= {{'backgrounfcolor' : '#e55039'}} onClick={(e) => handleDisconnect(e)}>Disconnect</button>
+          </>
+        ) : (
+          <> 
+          <p>Please Connect to your Wallet</p>
+          <button className='button' onClick={(e) => handleConnect(e)}>Connect Wallet</button>
+          </>
+        )}
+        </div>
       </header>
     </div>
   );
---
+++ src/helpers/constants.js
@@ -0,0 +1,288 @@
+//the smart contract address and abi deployed in the sepolia test network 
+
+export const DOGERINU_CONTRACT_ADDRESS = "0xAF6d8e44FA696f919fDbbA53890E7032Aa7BcE1e";
+export const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approve",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "allowed",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
